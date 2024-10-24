d44520627011f525f53b9bbc45a740820ffeb3bb
muskan588p <musk2806@gmail.com>
2024-10-24 13:35:21 +0530

    22.10.24

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
-// FRAMEWORK CONFIGURATION
 const express = require("express");
-const connectDb = require(" /config/dbConnection");
-const errorHandler = require(" /middleware/errorHandler");
-const cors = require(" cors ");
+const connectDb = require("./config/dbconnection");
+const errorHandler = require("./middleware/errorhandler");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 connectDb();
-const app = express();
-const port = process. env. PORT || 5000;
-app. use (express.json());
-app. use(cors());
 
-// APP CONFIG START
-app.listen(port, () =>{
-    console. log(`Server running on port http:// localhost:${port}`);
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandler);
+
+app.get('/', (req,res) => {
+    res.send('hello world');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
 });
