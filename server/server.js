2ae528dc947cd73f9cef5f1ed4162b06e36326d2
muskan588p <musk2806@gmail.com>
2024-11-04 10:20:25 +0530

    23oct

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const connectDb = require("./config/dbconnection");
 const errorHandler = require("./middleware/errorhandler");
 const cors = require("cors");
 
+const hbs = require("hbs");
+const path = require("path");
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -13,11 +17,36 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+// ERROR handling middleware
 app.use(errorHandler);
 
-app.get('/', (req,res) => {
-    res.send('hello world');
+app.set('view engine', 'hbs');
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
 });
+
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
